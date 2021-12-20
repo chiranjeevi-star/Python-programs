d9cfdf9cc9aba22b3402299eacb77d53b9e4d0fc
chiranjeevi-star <65226899+chiranjeevi-star@users.noreply.github.com>
2021-12-20 22:55:39 +0530

    Add files via upload

---
+++ python assignments.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Assignment-1
+
+# 1) In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.
+# 
+#  Ans) Values are : 'hello', -87.8, 6
+#       Expressions are: / , -, +     
+
+# 2) What is the difference between string and variable?
+# 
+# Ans) String is sequence of characters and represented in ''   eg: 'chiranjeevi','234'
+# 
+#      variable is container for storing the data   eg: a=23,b='chiru'
+#      
+
+# 3) Describe three different data types.
+# 
+# Ans) numeric data type: int,float,complex
+# 
+#      Boolean data type: True,False
+#      
+#      List data type: collection of elements
+
+# 4) What is an expression made up of? What do all expressions do?
+# 
+# Ans) An expression is a series of variables, operators, and method calls (constructed according to the syntax of the language). 
+# 
+# These expressions evaluates to a single value.
+
+# 5) This assignment statements, like spam = 10. What is the difference between an expression and a statement?
+# 
+# Ans) spam =10
+# 
+#      Expression is a combination of variables, operations and values that yields a result value.
+#      
+#      Statements represent an action or command e.g print statements, assignment statements
+# 
+
+# In[17]:
+
+
+# 6) After running the following code, what does the variable bacon contain?
+   
+bacon =22
+bacon+1
+
+# Ans) 23
+
+
+# In[12]:
+
+
+# 7) What should the values of the following two terms be?
+
+# Results of both are same
+
+print('spam' + 'spamspam')
+
+print('spam'* 3)
+
+
+# 8) Why is eggs a valid variable name while 100 is invalid?
+# 
+# Ans) variable name should not start with number so eggs is a valid variable but 100 is not valid 
+
+# In[14]:
+
+
+9) What three functions can be used to get the integer, floating-point number, or string
+get_ipython().set_next_input('version of a value');get_ipython().run_line_magic('pinfo', 'value')
+
+Ans) int(), float(),str()
+
+
+# 10) Why does this expression cause an error? How can you fix it?
+# 
+# 'I have eaten'+ 99 + 'burritos'
+# 
+# Ans) We can't concatenate string with integer. convert integer into string then only we can concatenate
+
+# In[ ]:
+
+
+
+
